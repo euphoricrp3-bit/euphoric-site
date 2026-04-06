266bdc79980f1a9e9cd7fa597c52963605bf7f9b
euphoricrp3-bit <euphoricrp3@gmail.com>
2026-04-05 21:44:19 -0400

    Update layout.tsx

--- app /layout.tsx
+++ app /layout.tsx
@@ -1,10 +1,15 @@
+export const metadata = {
+  title: "Euphoric RP",
+  description: "Euphoric RP Website"
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="en">
       <body>{children}</body>
     </html>
   );
